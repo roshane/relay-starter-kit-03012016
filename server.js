66f988d60ed62fe5c48686f5a7e742f0f960846e
roshane <roshanemadusanka@gmail.com>
2016-01-27 09:05:27 +0530

    resolve babel loader issue

--- server.js
+++ server.js
@@ -11,39 +11,44 @@ const GRAPHQL_PORT = 8080;
 // Expose a GraphQL endpoint
 var graphQLServer = express();
 graphQLServer.use('/', graphQLHTTP({
-  graphiql: true,
-  pretty: true,
-  schema: Schema,
+    graphiql: true,
+    pretty: true,
+    schema: Schema,
 }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
+    `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
 ));
 
 // Serve the Relay app
 var compiler = webpack({
-  entry: path.resolve(__dirname, 'js', 'app.js'),
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          plugins: ['./build/babelRelayPlugin'],
-        },
-        test: /\.js$/,
-      }
-    ]
-  },
-  output: {filename: 'app.js', path: '/'}
+    entry: path.resolve(__dirname, 'js', 'app.js'),
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: [
+                        require.resolve('babel-preset-es2015'),
+                        require.resolve('babel-preset-stage-0'),
+                        require.resolve('babel-preset-react')
+                    ],
+                    plugins: ['./build/babelRelayPlugin'],
+                },
+                test: /\.js$/,
+            }
+        ]
+    },
+    output: {filename: 'app.js', path: '/'}
 });
 var app = new WebpackDevServer(compiler, {
-  contentBase: '/public/',
-  proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
-  publicPath: '/js/',
-  stats: {colors: true}
+    contentBase: '/public/',
+    proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
+    publicPath: '/js/',
+    stats: {colors: true}
 });
 // Serve static resources
 app.use('/', express.static(path.resolve(__dirname, 'public')));
 app.listen(APP_PORT, () => {
-  console.log(`App is now running on http://localhost:${APP_PORT}`);
+    console.log(`App is now running on http://localhost:${APP_PORT}`);
 });
